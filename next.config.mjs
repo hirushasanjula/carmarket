e7399be56a9d56b63d8e5a6fe62ffea7317534d3
hirushasanjula <10898650@students.plymouth.ac.uk>
2025-03-11 23:47:59 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    server: {
+        port: process.env.PORT || 3000,
+      }
+};
 
 export default nextConfig;
