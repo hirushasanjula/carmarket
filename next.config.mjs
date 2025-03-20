5d56b3fa6eaa326793dcf3c2faa386dba718a37b
hirushasanjula <10898650@students.plymouth.ac.uk>
2025-03-21 01:01:24 +0530

    ,

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async headers() {
+        return [
+          {
+            source: '/(.*)',
+            headers: [
+              {
+                key: 'X-Forwarded-Host',
+                value: 'carmarket-leh4.onrender.com',
+              },
+            ],
+          },
+        ];
+      },
+};
 
 export default nextConfig;
